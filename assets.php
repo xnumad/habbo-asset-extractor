fd329c778a1c1ea5d90bd20f1152e77c950cfe9e
xnumad <34810600+xnumad@users.noreply.github.com>
2019-06-10 17:44:16 +0200

    Remove trailing slash from URL

--- assets.php
+++ assets.php
@@ -4,7 +4,7 @@ ini_set("display_errors", 1);
 set_time_limit(0);
 
 define('OUTPUT_DIRECTORY', dirname(__FILE__) . '/download/');
-define('OFFICIAL_RES_URL', "https://www.habbo.com/");
+define('OFFICIAL_RES_URL', "https://www.habbo.com");
 define('FLASH_CLIENT_URL', "http://habboo-a.akamaihd.net/gordon/PRODUCTION-201506301513-456128588/");
 define('DCR_URL', "http://habboo-a.akamaihd.net/dcr/hof_furni/");
 
